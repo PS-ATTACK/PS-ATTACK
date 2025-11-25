36ab4e38392a248f768c79b120cc9b8026f4b269
Sua Bae <bsa0530@gmail.com>
2025-11-25 21:06:42 +0900

    refactor(programmers): 17680 lfu deque사용하여 해결

--- programmers/17680.cc
+++ programmers/17680.cc
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <deque>
 #include <algorithm>
 
 using namespace std;
@@ -11,36 +12,35 @@ int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
     
     if(cacheSize == 0) {
+        // base condition
         return MISS * cities.size();
     }
+    
+    
     for(auto &s: cities) {
+        // cities string to lower case
         for_each(s.begin(), s.end(), [](auto &c) {c = tolower(c);});
     }
-    map<string, int> m;
     
-    for(auto& s: cities) {
-        if(m.count(s) /* cache hit */ ) {
+    // lfu datastructure
+    deque<string> dq;
+    
+    for(auto &s: cities) {
+        // find s in cache
+        auto it = find(dq.begin(), dq.end(), s);
+        if(it != dq.end()) {
+            // hit
             answer += HIT;
-        } else  {
+            dq.erase(it);
+            dq.push_back(s);
+        } else {
+            // miss
             answer += MISS;
-            if(m.size() >= cacheSize) /* delete */ {
-                int mn = 0;
-                string key;
-                for(auto& [k,v]: m) {
-                    if(v > mn) {
-                        mn = v;
-                        key = k;
-                    }
-                }
-                m.erase(key);
+            if(dq.size() == cacheSize) {
+                dq.pop_front();
             }
+            dq.push_back(s);
         }
-        for(auto& [k,v]: m) {
-            v++;
-        }
-        
-        m[s] = 1;
-        
     }
     
     return answer;
