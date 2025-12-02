2cc2150cb782510fddc9b62adbe160c285b4994d
Sua Bae <bsa0530@gmail.com>
2025-12-02 17:23:29 +0900

    Revert "[su8y] 문제풀이 (#7)"
    This reverts commit 749d799d02e7bad3556fba66a5434f6e1590da91.

--- users/su8y/leetcode/704.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        idx = bisect.bisect_left(nums, target)
-
-        if idx >= 0 and idx < len(nums) and nums[idx] == target: return idx 
-        return -1
